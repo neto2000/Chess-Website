c11ed4e3d176c78551ebac090d0d172a059c838a
neto2000 <82058954+neto2000@users.noreply.github.com>
2023-02-01 17:38:55 +0100

    added simple Hello World Web server

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router};
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main()
+{
+    let app = Router::new().route("/", get(handler));
+
+    let addr = SocketAddr::from(([127,0,0,1], 9000));
+
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+    
+}
+
+async fn handler() -> &'static str {
+    "Hello World!"
 }
