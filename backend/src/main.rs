df02efaf225129f5d7441451604489876921fdca
neto2000 <82058954+neto2000@users.noreply.github.com>
2023-02-03 20:27:13 +0100

    api: dict with all possible moves and image paths

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -26,7 +26,8 @@ async fn main()
         )
     }))
 
-    .route("/api", get(handler));
+    .route("/api", get(handler))
+    .route("/dict", get(get_figure_dict));
 
 
 
@@ -39,6 +40,34 @@ async fn main()
         .await
         .unwrap();
     
+}
+
+async fn get_figure_dict() -> impl IntoResponse
+{
+
+    // n, ne, e, ... stands for the cardinal points: north, north-east, east, ...
+    // the value stands for how long you can move in the direction (8 stands for over the complete field)
+
+    // auto-fill = true : you can move to all fields between the limit value and your position (eg. queen)
+    // auto-fill = false : you can only move to the point (eg. knight)
+
+    //every special move is hard coded into JavaScript
+
+    let pawn = json!({"start-pos": 5, "image": "./images/Bauer.svg", "n":1, "ne":0, "e":0,"se":0,"s":0,"sw":0,"w":0,"nw":0 , "auto-fill":false});
+
+    let queen = json!({"start-pos": 5, "image": "./images/Bauer.svg", "n":8, "ne":8, "e":8,"se":8,"s":8,"sw":8,"w":8,"nw":8 , "auto-fill":true});
+
+    let king = json!({"start-pos": 5, "image": "./images/Bauer.svg", "n":1, "ne":1, "e":1,"se":1,"s":1,"sw":1,"w":1,"nw":1 , "auto-fill":false});
+
+    let bishop = json!({"start-pos": 5, "image": "./images/Bauer.svg", "n":0, "ne":8, "e":0,"se":8,"s":0,"sw":8,"w":0,"nw":8 , "auto-fill":true});
+
+
+    // !! implement that it is not 2 in one direction but two in the direction and 1 left and right (in JS) !!
+    let knight = json!({"start-pos": 5, "image": "./images/Bauer.svg", "n":2, "ne":0, "e":2,"se":0,"s":2,"sw":0,"w":2,"nw":0 , "auto-fill":false});
+
+    let rook = json!({"start-pos": 5, "image": "./images/Bauer.svg", "n":8, "ne":0, "e":8,"se":0,"s":8,"sw":0,"w":8,"nw":0 , "auto-fill":true});
+
+    (StatusCode::OK, Json(json!({"pawn": pawn, "king": king, "queen": queen, "bishop": bishop, "knight": knight, "rook": rook})))
 }
 
 async fn handler() -> impl IntoResponse {
