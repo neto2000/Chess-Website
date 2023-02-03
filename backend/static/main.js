659ae3a58264fd60189e0e8cc9f69355f8374cd2
neto2000 <82058954+neto2000@users.noreply.github.com>
2023-02-03 20:26:08 +0100

    highlighting of possible paths

--- backend/static/main.js
+++ backend/static/main.js
@@ -1,4 +1,8 @@
 
+
+let figure_dict
+
+
 function CreateChessBoard()
 {
     let chess_board = document.getElementById("chess-board");
@@ -8,8 +12,6 @@ function CreateChessBoard()
 
     for (let i = 1; i <= 64; i++)
     {
-
-        console.log(counter);
 
         if (counter % 2 == 0)
         {
@@ -47,53 +49,126 @@ function CreateChessBoard()
     }
 }
 
-function Place_Figure(image, position)
+function Place_Figure(type, position)
 {
-    let field = document.getElementById("field" + position);
 
+    
+    let field = document.getElementById("field" + position);
+    
     let figure = document.createElement("button");
 
-    figure.className = "but";
+    figure.onclick = function()
+    {
 
 
+        let prev_highlights = document.getElementsByClassName("highlight-button");
+
+        for (let i = 0; i < prev_highlights.length; i++)
+        {
+            prev_highlights[i].remove();
+        }
+
+
+        calc_dict = {"n": -8, "ne": -7, "e": 1, "se": 9, "s": 8, "sw": 7, "w": -1, "nw": -9}
+
+
+        // spawn the highlight of all possible moves
+
+        let this_figure_dict = figure_dict[type]
+
+        let auto_fill = figure_dict["auto-fill"];
+
+        for (let key in this_figure_dict)
+        {
+            if(key != "start-pos" && key != "image" && key != "auto-fill")
+            {
+                if(auto_fill)
+                {
+
+                }
+                else
+                {
+                    
+                    
+                    if (this_figure_dict[key] != 0)
+                    {
+                        console.log(this_figure_dict[key] * calc_dict[key] + position);
+
+                        Place_Highlight(calc_dict[key] * this_figure_dict[key] + position, position);
+                    }
+
+                }
+            }
+        }
+    }
+    
+    figure.className = "figure-button";
+    // figure.id = "figure"
+    
+    
     let image_element = document.createElement("img");
     image_element.className = "figure-image";
-    image_element.setAttribute('src', image);
-
+    image_element.setAttribute('src', figure_dict[type]["image"]);
+    
     figure.appendChild(image_element);
-
+    
     field.appendChild(figure);
 }
 
-
-
-async function Click()
+function Place_Highlight(position, old_position)
 {
-    let data = await Get_Checklist_Data();
 
-    console.log(data);
+    if(position > 0)
+    {
+
+        let highlight_field = document.getElementById("field" + position);
+
+        if(highlight_field.firstElementChild == null)
+        {
+
+            let highlight_button = document.createElement("button");
+            highlight_button.className = "highlight-button";
+        
+            let highlight = document.createElement("img");
+        
+            highlight.className = "highlight-image";
+            highlight.setAttribute('src', "/images/highlight.svg");
+        
+            
+        
+            highlight_button.appendChild(highlight);
+        
+            highlight_field.appendChild(highlight_button);
+        }
+    
+    }
+
 }
 
 
 
 //simple GET request
-async function Get_Checklist_Data()
+async function Get_Figure_Dict()
 {
 
-    const response = await fetch("http://127.0.0.1:9000/api", {
+    const promise = await fetch("/dict", {
         method: "get"
     })
 
-    const data = await response.json()
+    const data = await promise.json();
 
-    return data
+    figure_dict = data;    
+    
+
 }
 
-window.onload = function(){ 
-    // document.getElementById("test-button").onclick = Click
+window.onload = async function(){ 
 
+    await Get_Figure_Dict(); 
     CreateChessBoard();
 
-    Place_Figure("./images/Bauer.svg", 3);
-    Place_Figure("./images/Bauer.svg", 20);
+    console.log(figure_dict);
+
+    Place_Figure("pawn", 33);
+    Place_Figure("pawn", 20);
 };
