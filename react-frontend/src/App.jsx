1dec0f6dc8f339fa432613156de43f97b2c04bde
neto2000 <82058954+neto2000@users.noreply.github.com>
2023-02-04 21:58:14 +0100

    migratred part of frontend to react

--- react-frontend/src/App.jsx
+++ react-frontend/src/App.jsx
@@ -1,34 +1,95 @@
-import { useState } from 'react'
+import { useState, useRef, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
-import './App.css'
+import "./App.css"
+
+let chess_board = [];
+
+let new_content = []
+
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+	for(let j = 1; j <= 64; j++)
+	{
+		new_content.push({id: j,html:(<></>)})
+	}
+
+	const [content, setContent] = useState(new_content);
+
+
+
+	let chess_board = []
+
+	let counter = 0;
+
+
+	for(let i = 1; i <= 64; i++)
+	{
+		
+
+		if(counter % 2 == 0)
+		{
+			chess_board.push((
+				<div className="square-even" key={i}>
+					{content[i-1].html}
+				</div>
+			));
+		}
+		else
+		{
+			chess_board.push((
+				<div className="square-odd" key={i}>
+					{content[i-1].html}
+				</div>
+			));
+		}
+
+		if (i % 8 == 0)
+		{
+			if(i != 0)
+			{
+				counter++;
+			}
+		}
+	
+		counter++;
+
+	}
+
+	
+
+	function PlaceFigure(field_pos)
+	{
+
+		const new_content = [...content]
+
+		const new_html = new_content.find(item => item.id === field_pos)
+
+		new_html.html = (
+		<button className='figure-button'>
+			<img className='figure-image' src='/images/Bauer.svg'></img>
+		</button>
+		);
+
+		setContent(new_content);
+	
+	}
+
+
+
+	return (
+		<div className="App">
+			<div className="board-container">
+				<div className="chess-board">
+					{chess_board}
+				</div>
+
+			</div>
+			<button onClick={PlaceFigure.bind(this, 4)}>Place</button>
+		</div>
+	)
 }
 
 export default App
