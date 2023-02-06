6a9cc276563605b398835b602d89c678c9e91ba3
neto2000 <82058954+neto2000@users.noreply.github.com>
2023-02-06 14:27:05 +0100

    completed migrting frontend to react

--- react-frontend/src/App.jsx
+++ react-frontend/src/App.jsx
@@ -2,14 +2,21 @@ import React, { useState, useRef, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import "./App.css"
 
-let chess_board = [];
 
 let new_content = []
 
-
+let figure_dict;
 
 
 function App() {
+
+	useEffect(() => {
+
+		console.log("Get Figure Dict")
+
+		Get_Figure_Dict();
+
+	}, [])
 
 
 	// create content State with empty HTML tag
@@ -83,34 +90,69 @@ function App() {
 	function PlaceHighlights(old_pos, type)
 	{
 
-		// Place all Highlights with the specifications of the type
+		console.log(figure_dict)
 
+		// Place all Highlights with the specifications of the type
 		const new_content = [...content]
 
-		const new_html = new_content.find(item => item.id === 5)
+		const calc_dict = {"n": -8, "ne": -7, "e": 1, "se": 9, "s": 8, "sw": 7, "w": -1, "nw": -9}
 
-
-		// check if a figure is on the field
-		if (new_html.html[0].type == "button")
-		{
-			new_html.html = [
-				<button className='figure-button' onClick={MoveToField.bind(this, old_pos, 5, type)}>
-					
-					<img className='highlight-image' src='/images/highlight.svg'></img>
-					
-					<img className='figure-image' src='/images/Bauer.svg'></img>
-				</button>
 		
-			];
-		}
-		else
-		{
+        let this_figure_dict = figure_dict[type]
+		
+        let auto_fill = figure_dict["auto-fill"];
+		
+        // spawn the highlight of all possible moves
+        for (let key in this_figure_dict)
+        {
+            if(key != "start-pos" && key != "image" && key != "auto-fill")
+            {
+                if(auto_fill)
+                {
 
-			new_html.html.push(
-			<button className='highlight-button'  onClick={MoveToField.bind(this, old_pos, 4, type)}>
-				<img className='highlight-image' src='/images/highlight.svg'></img>
-			</button>);
-		}
+                }
+                else
+                {
+                    
+                    if (this_figure_dict[key] != 0)
+                    {
+						
+						const current_highlight_pos = this_figure_dict[key] * calc_dict[key] + old_pos;
+
+                        console.log(current_highlight_pos);
+
+                        const new_html = new_content.find(item => item.id === current_highlight_pos)
+
+
+						// check if a figure is on the field
+						if (new_html.html[0].type == "button")
+						{
+							new_html.html = [
+								<button className='figure-button' onClick={MoveToField.bind(this, old_pos, current_highlight_pos, type)}>
+									
+									<img className='highlight-image' src='/images/highlight.svg'></img>
+									
+									<img className='figure-image' src='/images/Bauer.svg'></img>
+								</button>
+						
+							];
+						}
+						else
+						{
+
+							new_html.html.push(
+							<button className='highlight-button'  onClick={MoveToField.bind(this, old_pos, current_highlight_pos, type)}>
+								<img className='highlight-image' src='/images/highlight.svg'></img>
+							</button>);
+						}
+                    }
+
+                }
+            }
+        }
+
+
+		
 
 
 		setContent(new_content);
@@ -119,8 +161,19 @@ function App() {
 	function MoveToField(old_pos, field_pos, type)
 	{
 		console.log("Move")
+
 	}
 
+	async function Get_Figure_Dict()
+	{
+		const promise = await fetch("/dict", {
+			method: "get"
+		})
+	
+		const data = await promise.json();
+	
+		figure_dict = data;    
+	}
 
 	return (
 		<div className="App">
@@ -130,7 +183,7 @@ function App() {
 				</div>
 
 			</div>
-			<button onClick={PlaceFigure.bind(this, 4)}>Place</button>
+			<button onClick={PlaceFigure.bind(this, 10, "pawn")}>Place</button>
 		</div>
 	)
 }
