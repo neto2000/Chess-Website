9d236a8f396dab9b2126e801b1183541b856a820
neto2000 <82058954+neto2000@users.noreply.github.com>
2023-07-27 20:04:13 +0200

    removed unused files

--- svelte-frontend/src/lib/check.svelte
+++ svelte-frontend/src/lib/check.svelte
@@ -103,25 +103,34 @@
     check_position("Knight",  own_team, enemy_team, field_array, {x: position.x + 1, y: position.y - 2})
 
 
-    console.log(check_list)
+
+
+
   }
 
 
   function check_position(type, own_team,enemy_team, field_array, position) {
 
     if(position.y < 0 || position.y > 7 || position.x < 0 || position.x > 7) {
+      
+      console.log("over borders!")
+
       return false
     }
 
     //console.log("Y: " + position.y + ", " + "X: " + position.x);
 
     if(field_array[position.y][position.x].team == own_team) {
+
       console.log("own team!")
+
       return false
     }
 
-    if(field_array[position.y][position.x].type == type && field_array[position.y][position.x].team == enemy_team) {
-        check_list[0] = true
+    if(field_array[position.y][position.x].figure == type && field_array[position.y][position.x].team == enemy_team) {
+      console.log("chess")  
+
+      check_list[0] = true
 
         check_list.push({type: type, pos: position})
 
